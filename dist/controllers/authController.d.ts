b91fc6b072028cbe424846aef40cf30f0345953a
Eduardo Ferraz <edupferraz@gmail.com>
2025-09-07 03:12:57 -0300

    chore: remove dist from repository

--- dist/controllers/authController.d.ts
+++
@@ -1,5 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export declare class AuthController {
-    static login(req: Request, res: Response, next: NextFunction): Promise<void>;
-}
-//# sourceMappingURL=authController.d.ts.map
--- dist/middleware/validation.d.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { Schema } from 'joi';
-export declare const validateBody: (schema: Schema) => (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateQuery: (schema: Schema) => (req: Request, res: Response, next: NextFunction) => void;
-export declare const validateParams: (schema: Schema) => (req: Request, res: Response, next: NextFunction) => void;
-//# sourceMappingURL=validation.d.ts.map
