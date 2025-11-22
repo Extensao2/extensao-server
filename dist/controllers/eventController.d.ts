996bf215af34e31ae0a42de23a30ab91a5607a72
Henrique Patricio <127338504+vlghpp@users.noreply.github.com>
2025-11-22 08:15:07 -0300

    ad-hoc: remove dist folder (#14)
    * chore: update .gitignore to add dist folder ignore
    * fix: remove dist folder

--- dist/controllers/eventController.d.ts
+++
@@ -1,7 +0,0 @@
-import { Response, NextFunction } from 'express';
-import { AuthenticatedRequest } from '@/types';
-export declare class EventController {
-    static getAllEvents(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void>;
-    static getEventsByUserId(req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void>;
-}
-//# sourceMappingURL=eventController.d.ts.map
--- dist/utils/jwt.d.ts
+++
@@ -1,9 +0,0 @@
-import { JWTPayload } from '@/types';
-export declare class JWTUtils {
-    private static secret;
-    private static expiresIn;
-    static generateToken(payload: Partial<JWTPayload>): string;
-    static verifyToken(token: string): JWTPayload;
-    static verifyOAuthToken(token: string): JWTPayload;
-}
-//# sourceMappingURL=jwt.d.ts.map
