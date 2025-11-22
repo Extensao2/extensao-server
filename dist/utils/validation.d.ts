360eb189373f5da2cda03801704b84a0fb602352
matheus <matheusglautert@gmail.com>
2025-11-22 09:18:02 -0300

    chore: remover dist do versionamento

--- dist/utils/validation.d.ts
+++
@@ -1,7 +0,0 @@
-import Joi from 'joi';
-export declare const loginSchema: Joi.ObjectSchema<any>;
-export declare const resourceCreateSchema: Joi.ObjectSchema<any>;
-export declare const resourceUpdateSchema: Joi.ObjectSchema<any>;
-export declare const searchSchema: Joi.ObjectSchema<any>;
-export declare const uuidSchema: Joi.StringSchema<string>;
-//# sourceMappingURL=validation.d.ts.map
