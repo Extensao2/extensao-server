deedb6120cff85b28fd8f726438401d020c9c842
Henrique <henriquepworking@gmail.com>
2025-12-03 21:09:48 -0300

    refactor: remove mock in server.js

--- src/server.js
+++ src/server.js
@@ -54,20 +54,6 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-if (process.env.NODE_ENV !== 'production') {
-  app.use((req, res, next) => {
-    if (!req.user) {
-      req.user = {
-        _id: '000000000000000000000000', // opcional, só pra manter formato
-        email: 'mock.user@example.com',
-        name: 'Usuário Mock',
-      };
-    }
-    next();
-  });
-}
-
 // Routes
 app.use('/api/v1', authRoutes);
 app.use('/api/v1', resourceRoutes);
